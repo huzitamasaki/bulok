dddbbf0a39a4e5b412d97f6ea84d0c175dd84801
kumagawa <vantan.zyugyounaiyou@gmail.com>
2021-09-04 13:54:05 +0900

    UIの修正

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            pos = Input.GetTouch(0).position;
+            Debug.Log("pos=" + pos);
+        }
+    }
+}
